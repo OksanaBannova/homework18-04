a7f65efd5113fde426e4a42087fadc6bcc69594f
Oksana Bannova <oksanchik2170@yandex.ru>
2024-04-20 23:21:08 +0700

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.txt$/,
+                loader: 'raw-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCSSExtractPlugin.loader,
+                    'css-loader'
+                ]
+        },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin(),
+    
+        new MiniCSSExtractPlugin()
+    ]
+    }
